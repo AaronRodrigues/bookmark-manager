1519eee10fab4719615d1cb4faddb3021fc39cdc
Mário Ribeiro <mail@marioribeiro.com>
2017-06-01 15:09:07 +0100

    Add final changes to step 15

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,6 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
-require 'data_mapper'
-require 'dm-postgres-adapter'
-require './app/models/link'
-require './app/models/tag'
-require './app/data_mapper_setup'
+require_relative 'data_mapper_setup'
 
 class Bookmark < Sinatra::Base
 
@@ -18,9 +14,10 @@ class Bookmark < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.create(url: params[:URL], title: params[:title])
-    tag = Tag.create(name: params[:tags])
+    link = Link.new(url: params[:URL], title: params[:title])
+    tag = Tag.first_or_create(name: params[:tags])
     link.tags << tag
+    link.save
     redirect '/links'
   end
 
